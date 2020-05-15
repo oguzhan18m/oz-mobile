dfef609298a5a791dea280e8edc8955863f74331
oguzhan18m <62189492+oguzhan18m@users.noreply.github.com>
2020-05-15 23:12:59 +0300

    update1

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -21,7 +21,7 @@ export default class Cart extends Component {
                                     <hr/>
                                     <CartColumns />
                                     <CartList value={value} />
-                                    <CartTotals value={value} />
+                                    <CartTotals value={value} history={this.props.history}/>
                                 </React.Fragment>
                                 
                             );
